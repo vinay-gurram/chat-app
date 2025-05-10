e2bbd00b6b2f9a737c44816d68c6307b2b33386e
vinay-gurram <gurramvinay144@gmail.com>
2025-05-10 21:57:15 +0530

    first commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getUserForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
+
+
+
+const router = express.Router();
+
+
+router.get("/users", protectRoute, getUserForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage)
+
+
+export default router;
