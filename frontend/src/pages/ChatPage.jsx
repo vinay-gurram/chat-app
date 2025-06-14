9bbb6498e50adc62e5b38838bd374867d70e631b
vinay-gurram <gurramvinay144@gmail.com>
2025-06-14 14:04:12 +0530

    changed SaidBar

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,7 @@
 import { useChatStore } from "../store/useChatStore";
 
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
+
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
