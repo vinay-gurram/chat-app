04d7103d9b2414603ff0a690395cb615d9777950
vinay-gurram <gurramvinay144@gmail.com>
2025-06-15 10:59:33 +0530

    gits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsx: 'automatic',
-  },
+  plugins: [
+    react({
+      jsxRuntime: "automatic", // Enables automatic JSX transform (no need to import React)
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://chat-app-1-ci2r.onrender.com',
+      "/api": {
+        target: "https://chat-app-1-ci2r.onrender.com",
         changeOrigin: true,
         secure: false,
       },
