933fff250569159dd81d634821497d6409dbcdaa
vinay-gurram <gurramvinay144@gmail.com>
2025-06-16 07:29:08 +0530

    gits

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // ✅ import AuthProvider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider> {/* ✅ Wrap App with AuthProvider */}
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
